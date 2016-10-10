55be31d2ee3ab8afbfcc34a98ec8d0bccd1219a9
antoniopol06 <antonio.pol@liferay.com>
2016-10-10 15:41:17 +0200

    Test component states

--- test/LexiconSticker.js
+++ test/LexiconSticker.js
@@ -42,4 +42,82 @@ describe('LexiconSticker', function() {
 
 	});
 
+	it('should generate sticker with level as primary', function() {
+		sticker = new LexiconSticker(
+			{
+				label: '123',
+				level: 'primary'
+			}
+		);
+
+		assert.strictEqual(sticker.element.outerHTML, __html__['test/fixture/testLexiconStickerLevel.html']);
+	});
+
+	it('should generate sticker with outside', function() {
+		sticker = new LexiconSticker(
+			{
+				label: '123',
+				outside: true,
+				position: 'top-right'
+			}
+		);
+
+		assert.strictEqual(sticker.element.outerHTML, __html__['test/fixture/testLexiconStickerOutside.html']);
+	});
+
+	it('should generate sticker with position', function() {
+		sticker = new LexiconSticker(
+			{
+				label: '123',
+				position: 'bottom-right'
+			}
+		);
+
+		assert.strictEqual(sticker.element.outerHTML, __html__['test/fixture/testLexiconStickerPosition.html']);
+	});
+
+	it('should generate sticker with shape', function() {
+		sticker = new LexiconSticker(
+			{
+				label: '123',
+				shape: 'rounded'
+			}
+		);
+
+		assert.strictEqual(sticker.element.outerHTML, __html__['test/fixture/testLexiconStickerShape.html']);
+	});
+
+	it('should generate sticker with size', function() {
+		sticker = new LexiconSticker(
+			{
+				label: '123',
+				size: 'lg'
+			}
+		);
+
+		assert.strictEqual(sticker.element.outerHTML, __html__['test/fixture/testLexiconStickerSize.html']);
+	});
+
+	it('should generate static sticker', function() {
+		sticker = new LexiconSticker(
+			{
+				label: '123',
+				static: true
+			}
+		);
+
+		assert.strictEqual(sticker.element.outerHTML, __html__['test/fixture/testLexiconStickerStatic.html']);
+	});
+
+	it('should generate sticker with custom css', function() {
+		sticker = new LexiconSticker(
+			{
+				label: '123',
+				elementClasses: 'my-custom-css'
+			}
+		);
+
+		assert.strictEqual(sticker.element.outerHTML, __html__['test/fixture/testLexiconStickerCustomCss.html']);
+	});
+
 });
